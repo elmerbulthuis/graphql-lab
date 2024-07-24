0deecd81b2c052f52fa818e36975c37ece0ffbae
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-07-24 13:33:50 +0200

    product with type-graphql

---
+++ src/product.test.ts
@@ -0,0 +1,85 @@
+import { graphql } from "graphql";
+import assert from "node:assert";
+import test from "node:test";
+import { ProductContext, ProductInputModel, ProductModel, productSchema } from "./product.js";
+
+test("user", async () => {
+  const createProductSource = `
+    mutation ($model: ProductInputModel!) {
+      createProduct(model: $model)
+    }
+  `;
+  const getProductSource = `
+    query ($key: Int!) {
+      getProduct(key: $key) {
+        name, description, price
+      }
+    }
+  `;
+
+  const context = new ProductContext();
+
+  {
+    const result = await graphql({
+      contextValue: context,
+      schema: productSchema,
+      source: createProductSource,
+      variableValues: {
+        model: {
+          name: "Box",
+          description: "lorem ipsum",
+          price: 1.0,
+        } satisfies ProductInputModel,
+      },
+    });
+    assert.deepEqual(result, { data: { createProduct: 1 } });
+  }
+
+  {
+    const result = await graphql({
+      contextValue: context,
+      schema: productSchema,
+      source: createProductSource,
+      variableValues: {
+        model: {
+          name: "Triangle",
+          description: null,
+          price: 2.0,
+        } satisfies ProductInputModel,
+      },
+    });
+    assert.deepEqual(result, { data: { createProduct: 2 } });
+  }
+
+  {
+    const result = await graphql({
+      contextValue: context,
+      schema: productSchema,
+      source: getProductSource,
+      variableValues: {
+        key: 1,
+      },
+    });
+    assert.deepEqual(result, {
+      data: {
+        getProduct: { name: "Box", description: "lorem ipsum", price: 1.0 } satisfies ProductModel,
+      },
+    });
+  }
+
+  {
+    const result = await graphql({
+      contextValue: context,
+      schema: productSchema,
+      source: getProductSource,
+      variableValues: {
+        key: 2,
+      },
+    });
+    assert.deepEqual(result, {
+      data: {
+        getProduct: { name: "Triangle", description: null, price: 2.0 } satisfies ProductModel,
+      },
+    });
+  }
+});
--- src/product.ts
+++ src/product.ts
@@ -1,19 +1,92 @@
-import { buildSchema } from "graphql";
+import { GraphQLFloat, GraphQLInt, GraphQLString } from "graphql";
+import "reflect-metadata";
+import {
+  Arg,
+  buildSchema,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
 
-const productSchema = buildSchema(
-  `
-    type Product {
-      name: String!
-      description: String
-      price: Float!
-    }
-    
-    type Mutation {
-      createProduct(input: Product!): Int
-    }
-    
-    type Query {
-      getProduct(input: Int!): Product
-    }
-  `,
-);
+// const productSchema = buildSchema(
+//   `
+//     type Product {
+//       name: String!
+//       description: String
+//       price: Float!
+//     }
+
+//     type Mutation {
+//       createProduct(input: Product!): Int
+//     }
+
+//     type Query {
+//       getProduct(input: Int!): Product
+//     }
+//   `,
+// );
+
+export class ProductContext {
+  private lastKey = 0;
+  private readonly store = new Map<number, ProductModel>();
+
+  public get(key: number) {
+    return this.store.get(key);
+  }
+
+  public insert(model: ProductModel): number {
+    const key = ++this.lastKey;
+    this.store.set(key, model);
+    return key;
+  }
+}
+
+@ObjectType()
+export class ProductModel {
+  @Field()
+  name!: string;
+
+  @Field(() => GraphQLString, { nullable: true })
+  description!: string | null;
+
+  @Field(() => GraphQLFloat)
+  price!: number;
+}
+
+@InputType()
+export class ProductInputModel {
+  @Field()
+  name!: string;
+
+  @Field(() => GraphQLString, { nullable: true })
+  description!: string | null;
+
+  @Field(() => GraphQLFloat)
+  price!: number;
+}
+
+@Resolver(ProductModel)
+export class ProductResolver {
+  @Query(() => ProductModel)
+  async getProduct(@Ctx() context: ProductContext, @Arg("key", () => GraphQLInt) key: number) {
+    const model = context.get(key);
+    return model;
+  }
+
+  @Mutation(() => GraphQLInt)
+  async createProduct(
+    @Ctx() context: ProductContext,
+    @Arg("model") model: ProductInputModel,
+  ): Promise<number> {
+    const key = context.insert(model);
+    return key;
+  }
+}
+
+export const productSchema = await buildSchema({
+  resolvers: [ProductResolver],
+});
