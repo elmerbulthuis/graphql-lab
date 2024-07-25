7614bb961c9555d03ad281a4fdced57ea51761c4
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-07-25 18:59:55 +0200

    use apollo client

--- src/hi-there.test.ts
+++ src/hi-there.test.ts
@@ -1,10 +1,11 @@
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client/core/core.cjs";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import assert from "node:assert";
 import test from "node:test";
 import { hiThereSchema } from "./hi-there.js";
 
-test("hi-there", async () => {
+test("hi-there fetch", async () => {
   const hiThereSource = `
     query {
       hiThere {
@@ -34,8 +35,41 @@ test("hi-there", async () => {
     const result = (await response.json()) as any;
 
     assert.ifError(result.errors);
+    assert.deepEqual(result.data, { hiThere: { message: "hi there" } });
+  } finally {
+    controller.abort();
+  }
+});
 
-    assert.deepEqual(result, { data: { hiThere: { message: "hi there" } } });
+test("hi-there apollo client", async () => {
+  const hiThereSource = gql`
+    query {
+      hiThere {
+        message
+      }
+    }
+  `;
+
+  const controller = new AbortController();
+  try {
+    const server = new ApolloServer({
+      schema: hiThereSchema,
+    });
+    const { url: baseUrl } = await startStandaloneServer(server, {
+      listen: { signal: controller.signal, port: 8080 },
+    });
+
+    const cache = new InMemoryCache();
+    const client = new ApolloClient({ uri: baseUrl, cache });
+
+    const result = await client.query({
+      query: hiThereSource,
+    });
+
+    assert.ifError(result.errors);
+    assert.deepEqual(result.data, {
+      hiThere: { __typename: "HiThereModel", message: "hi there" },
+    });
   } finally {
     controller.abort();
   }
